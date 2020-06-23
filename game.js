7b736f5ed41a55254610268729dbfb31931d57f1
MMMarz009 <mar0099887@gmail.com>
2020-06-23 16:02:33 -0500

    To submit something at least before forget

--- game.js
+++ game.js
@@ -1,38 +1,43 @@
 // Game logic 
 // Variables 
-    // timer 
-
-    // score?? 
-var startTime = document.querySelector('#startBtn');
+var startTime = document.querySelector('.startBtn');
 var timer=document.querySelector("#timer");
 var choices = document.querySelector("#choices");
 var seconds= document.querySelector('#seconds');
 var questions= document.querySelector("#questions");
 
+
 function startQuiz(){
-    // timer start
-    // text content of timer on DOM 
     startTime.addEventListener("click",function(){
-            alert("you have started. RIP");
-            event.preventDefault();
-            getQuestion();
-        });
+        event.preventDefault();
+        console.log("you have started. RIP");
+        startTimer();
+        getQuestion();
+    });
+};
 
 // When answer a questions then move on to another question
-	// get the question/answer object from your questions array based on the current question index
+// get the question/answer object from your questions array based on the current question index
 	// update the DOM with the current question
-  // clear out any old question choices  
-  // update the DOM with the current answer choices
+    // clear out any old question choices  
+    // update the DOM with the current answer choices
 			// for loop that goes through the answer choices
 				// creates a button
 				// set a class attribute of "choice"
 				// set a value attribute of choice
 				// set text content of button to be choice
                 // append button to the DOM 
-function getQuestion (arr){
-    for (var i=0; i< arr.length;i++){
-        choices.addEventListener("click",function());
-    };
-    
-                
+function getQuestion(){
+    <script type="text/javascript" src="questionAnswer.js"></script>
+    for (let i = 0; i < questionAnswers.length; i++) {
+        
+        const element = questionAnswers[index];
+        
+    }
 };
+
+function startTimer(){
+    seconds= setInterval(function(){
+       seconds 
+    })
+}
--- score.js
+++ score.js
@@ -1,17 +1,29 @@
 
 // variables 
-	var score=[];
-// positve
-// negative
-// When a question is answered correctly, add points 
+	var score= [];
+	var scoreScreen = [];
 
-// When a question is answered incorrectly, subtract time from the clock 
+localStorage.setItem("score",JSON.stringify(score));
+
+var retrieveScores = JSON.parse(localStorage.getItem("score"));
+retrieveScores.sort(function(a,b){
+	return b.score - a.score;
+});
+
+for(var i=0;i<retrieveScores.length;i++){
+	var scoreDiv = document.createElement("div");
+	scoreDiv.textContent=retrieveScores[i]+" - "+retrieveScores[i].score
+}
+
 
 function checkAnswer() {
 	// if choice value is incorrect
+	
 		// subtract seconds from the timer
 		// display new time to page
 		// let player know they are incorrect
+
+
 	// else 
 		// let the player know they're correct
 
